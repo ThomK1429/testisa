99e9cf7d13470542c3e7fd2ee2003cd0da1bbe44
ThomK1429 <sndbytgkv1@verizon.net>
2019-09-04 15:15:32 -0400

    remove local mysql  connect

--- server.js
+++ server.js
@@ -46,22 +46,22 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 // Here I am connecting to the mysql host 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "root",
-  database: "notetaker_db"
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "root",
+//   database: "notetaker_db"
+// });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
+// connection.connect(function(err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
 
-  console.log("connected as id " + connection.threadId);
-});
+//   console.log("connected as id " + connection.threadId);
+// });
 
 // create route and establish connection from root source
 app.get("/", function(req, res) {
